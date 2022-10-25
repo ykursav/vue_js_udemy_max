35926e29e1969cc2342be2ce3b7682cc9c10b5fa
Yunus Kursav <yueku@orsted.com>
2022-10-25 13:31:38 +0200

    Finished getting started

--- 001_GettingStarted/gs-01-starting-project/app.js
+++ 001_GettingStarted/gs-01-starting-project/app.js
@@ -1,12 +1,25 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+app = Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+    },
+  },
+}).mount("#app");
 
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
 
-buttonEl.addEventListener('click', () => {
-    const enteredValue = inputEl.value;
-    const listItemEl = document.createElement('li');
-    listItemEl.textContent = enteredValue;
-    listEl.appendChild(listItemEl);
-    inputEl.value = "";
-});
+// buttonEl.addEventListener('click', () => {
+//     const enteredValue = inputEl.value;
+//     const listItemEl = document.createElement('li');
+//     listItemEl.textContent = enteredValue;
+//     listEl.appendChild(listItemEl);
+//     inputEl.value = "";
+// });
